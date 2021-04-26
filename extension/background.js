b3b3ea8b42a72aaa33f0e7a272cba6088b658cd0
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2021-04-26 13:43:47 +0200

    Add files via upload

---
+++ extension/background.js
@@ -0,0 +1,6 @@
+let color = 'rgb(15, 155, 155)';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
---
+++ extension/popup.js
@@ -0,0 +1,45 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+let tempspan = document.getElementById("demo");
+let weaterbtn = document.getElementById("gettemp");
+let updatetext = document.getElementById("lastupdate");
+
+
+
+let getTemp = async () => {
+  req = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=66614,us&units=metric&appid=db6608063a9d72758e29ea323da07bd1');
+  res = await req.json();
+  let newdate = new Date();
+  let lastupdate = newdate;
+  tempspan.innerText = res.main.temp;
+  updatetext.innerText = (newdate.getMonth() + 1) + "-" + newdate.getDate() + "-" + newdate.getFullYear();
+}
+
+weaterbtn.addEventListener("click", async () => {
+   getTemp();
+});
+
+
+
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+
+
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
